416d627b3a65486bbc161aedaa4bd4f49f09feb6
luis <foreverluiscastro@gmail.com>
2021-05-12 15:44:16 -0400

    commit tree bf6525712900c7cd06088d89305faa9d2bb86331

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  arr = []
+  array.each{|number| arr << number ** 2}
+  arr
 end
+
+# def square_array(array)
+#   arr = array.collect{|number| number ** 2}
+#   arr
+# end
